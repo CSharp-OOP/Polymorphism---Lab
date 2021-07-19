f40e61f342785d20846b013dd32316ff148ccb55
remzi chaushev <remzitdf@abv.bg>
2021-07-19 09:59:54 +0300

    Add project files.

---
+++ 03.Shapes/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; set; }
+
+        public override double CalculateArea() => Math.PI * radius * radius;
+
+        public override double CalculatePerimeter() => 2 * Math.PI * radius;
+
+        public override string Draw() => base.Draw() + this.GetType().Name;
+    }
+}
---
+++ 03.Shapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public override double CalculateArea() => Height * Width;
+
+        public override double CalculatePerimeter() => 2 * (Height + Width);
+
+        public override string Draw() => base.Draw() + this.GetType().Name;
+    }
+}
